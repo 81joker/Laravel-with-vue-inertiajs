146a5c9a5c06a9d798b072673a167dab6f84286b
81joker <nehad.al.timimi@gmail.com>
2024-10-24 13:24:19 +0200

    make MainLayout global

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,11 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
-    <Link href="/hello">Show Page</Link>
+    <Link href="/show">Show Page</Link>
     <div>The page with time {{timer}}</div>
     <slot>Default</slot>
   </template>
   <script setup>
-  import { Link } from '@inertiajs/inertia-vue3'
+  import { Link } from '@inertiajs/vue3'
   import { ref } from 'vue'
 
 
