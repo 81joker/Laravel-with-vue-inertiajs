c5dc0466f178db7ca68714d5c1eabe46c361e777
Nehad <35563834+81joker@users.noreply.github.com>
2024-10-28 12:50:08 +0100

    Create AuthController + Route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 
@@ -16,5 +17,11 @@ use App\Http\Controllers\ListingController;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/show', [IndexController::class, 'show']);
 Route::resource('listing', ListingController::class);
-  // ->only(['index', 'show', 'create']);
-  
+// ->only(['index', 'show', 'create']);
+
+Route::get('login', [AuthController::class, 'create'])
+  ->name('login');
+Route::post('login', [AuthController::class, 'store'])
+  ->name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy'])
+  ->name('logout');
