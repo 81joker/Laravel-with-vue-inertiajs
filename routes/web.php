122fe520d24f1778447cba624962efa9c2e46ca9
Nehad <35563834+81joker@users.noreply.github.com>
2024-11-07 01:24:08 +0100

    created RealtorListing controller + route + view

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserAccountController;
+use App\Http\Controllers\RealtorListingController;
 
 
 Route::get('/', [IndexController::class, 'index']);
@@ -25,3 +26,10 @@ Route::post('login', [AuthController::class, 'store'])
 
   Route::resource('user-account', UserAccountController::class)
   ->only(['create','store']);
+  
+  Route::prefix('realtor')
+  ->name('realtor.')
+  ->middleware('auth')
+  ->group(function () {
+    Route::resource('listing', RealtorListingController::class);
+  });
