12097a6efd1910d7748e696f1d8ad6267e2b0891
81joker <nehad.al.timimi@gmail.com>
2024-12-06 09:27:00 +0100

    add progress and spinner to website

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,10 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
 import { ZiggyVue  } from 'ziggy-js';
 import { Ziggy } from './ziggy.js';
-
-
-
+// import { Inertia } from '@inertiajs/inertia';
+import NProgress from 'nprogress'
+import { router } from '@inertiajs/vue3'
+router.on('start', () => NProgress.start())
 
 createInertiaApp({
   resolve: name => {
@@ -14,8 +15,8 @@ createInertiaApp({
     const page =  pages[`./Pages/${name}.vue`]
     page.default.layout = page.default.layout || MainLayout
     return page
-    
   },
+
   progress: {
     // The delay after which the progress bar will appear, in milliseconds...
     delay: 250,
